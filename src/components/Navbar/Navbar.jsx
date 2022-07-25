23aae9fa8f90e5611104532e6cf80c80a6843116
scottkelly36 <scottkelly36@gmail.com>
2022-07-25 11:09:35 +0100

    started layout

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navlink } from 'react-router-dom';
+
+const Navbar = () => {
+	return (
+		<nav>
+			<Navlink></Navlink>
+		</nav>
+	);
+};
+
+export default Navbar;
