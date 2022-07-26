7b69cb442653950f9be205692dddd8af7ffa5335
scottkelly36 <scottkelly36@gmail.com>
2022-07-26 11:03:35 +0100

    removed console logs

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,8 @@
-import { useEffect, useState, Fragment } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Fragment } from 'react';
+
 import './Home.css';
 
 const Home = () => {
-	const [isLoading, setLoading] = useState(true);
-	const [allReviews, setAllReviews] = useState([]);
-
-	useEffect(() => {
-		axios
-			.get(
-				'https://scotts-game-app.herokuapp.com/api/reviews'
-			)
-			.then((res) => {
-				setAllReviews(res.data.reviews);
-				setLoading(false);
-			});
-	}, []);
-	console.log(allReviews);
 	return (
 		<Fragment>
 			<section className='container'>
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,49 +1,60 @@
-import {Fragment, useState, useEffect} from 'react'
+import { Fragment, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Loading from '../Loading/Loading.jsx';
 import axios from 'axios';
-import './Review.css'
-
+import './Review.css';
 
 const Review = () => {
+	const { review_id } = useParams();
+	const [review, setReview] = useState([]);
+	const [isLoading, setLoading] = useState(true);
 
-const {review_id} = useParams();
-const [review, setReview] = useState([]);
-const [isLoading, setLoading] = useState(true);
+	useEffect(() => {
+		axios
+			.get(
+				`https://scotts-game-app.herokuapp.com/api/reviews/${review_id}`
+			)
+			.then((res) => {
+				setReview(res.data.review);
+				setLoading(false);
+			});
+	}, [review_id]);
 
-useEffect(()=>{
-    axios.get(`https://scotts-game-app.herokuapp.com/api/reviews/${review_id}`).then((res)=>{
-        setReview(res.data.review)
-        setLoading(false);
-    })
-},[review_id]);
+	return isLoading ? (
+		<Loading />
+	) : (
+		<Fragment>
+			<heading className='review-heading'>
+				<h1>{review.title}</h1>
+			</heading>
+			<section className='review-container'>
+				<img
+					src={review.review_img_url}
+					className='review-img'
+					alt=''
+				/>
+				<div className='review-info-container'>
+					<p className='review-category'>
+						{review.category}
+					</p>
+					<p className='review-designer'>
+						Designed by {review.designer}
+					</p>
+					<p className='review-author'>
+						Review by {review.owner}
+					</p>
+					<p className='review-body'>
+						{review.review_body}
+					</p>
+					<div className='vote-btns'>
+						<button className='btn btn-feature'>+</button>
+						<button className='btn btn-danger'>-</button>
+					</div>
+					<p className='votes'>{review.votes}</p>
+				</div>
+			</section>
+		</Fragment>
+	);
+};
 
-console.log(review)
-
-  return (
-    isLoading? <Loading/> : 
-    <Fragment>
-    <heading className="review-heading">
-    <h1>{review.title}</h1>
-    </heading>
-    <section className='review-container'>
-        <img src={review.review_img_url} className="review-img" alt="" />
-        <div className="review-info-container">
-            <p className="review-category">{review.category}</p>
-            <p className="review-designer">Designed by {review.designer}</p>
-            <p className="review-author">Review by {review.owner}</p>
-            <p className="review-body">{review.review_body}</p>
-            <div className="vote-btns">
-                <button className='btn btn-feature'>+</button>
-                <button className='btn btn-danger'>-</button>
-            </div>
-            <p className="votes">{review.votes}</p>
-        </div>
-        
-    </section> 
-    </Fragment>
-    
-  )
-}
-
-export default Review
+export default Review;
